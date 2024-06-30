b2f86b47d3a6110c309228a8b922267101b16f64
leobewater <mmbee888@gmail.com>
2024-06-30 09:12:53 -0400

    add search filters

--- djangobnb/app/components/navbar/Navbar.tsx
+++ djangobnb/app/components/navbar/Navbar.tsx
@@ -1,7 +1,28 @@
+import SearchFilters from '@/app/components/navbar/SearchFilters';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
-  return <nav className='w-full fixed top-0 left-0 py-6 border-b bg-white z-10'>Navbar</nav>;
+  return (
+    <nav className='w-full fixed top-0 left-0 py-6 border-b bg-white z-10'>
+      <div className='max-w-[1500px] mx-auto px-6'>
+        <div className='flex justify-between items-center'>
+          <Link href='/'>
+            <Image src='/logo.png' alt='DjangoBnb' width={180} height={38} />
+          </Link>
+
+          <div className="flex space-x-6">
+            <SearchFilters />
+          </div>
+
+          <div className="flex items-center space-x-6">
+            Add property - User nav
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ djangobnb/app/components/navbar/SearchFilters.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchFilters = () => {
+  return <div>SearchFilters</div>;
+};
+
+export default SearchFilters;
