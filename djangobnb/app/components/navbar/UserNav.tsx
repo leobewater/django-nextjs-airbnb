559f307a3337fe85d5b62f23be4ed6f1a0dbfa52
leobewater <mmbee888@gmail.com>
2024-06-30 15:23:48 -0400

    toggle user menu

--- djangobnb/app/components/navbar/UserNav.tsx
+++ djangobnb/app/components/navbar/UserNav.tsx
@@ -4,11 +4,11 @@ import MenuLink from '@/app/components/navbar/MenuLink';
 import { useState } from 'react';
 
 const UserNav = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className='p-2 relative inline-block border rounded-full'>
-      <button className='flex items-center'>
+      <button onClick={() => setIsOpen(!isOpen)} className='flex items-center'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
