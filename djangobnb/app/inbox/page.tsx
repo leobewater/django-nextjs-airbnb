0531693dff25a40dab87d07bc77d318f7aae0cd6
leobewater <mmbee888@gmail.com>
2024-06-30 15:01:00 -0400

    add inbox page

---
+++ djangobnb/app/inbox/page.tsx
@@ -0,0 +1,15 @@
+import Conversation from '@/app/components/inbox/Conversation';
+import React, { useState, useEffect } from 'react';
+
+const InboxPage = async () => {
+  return (
+    <main className='max-w-[1500px] mx-auto px-6 pb-6 space-y-4'>
+      <h1 className='my-6 text-2xl'>Inbox</h1>
+      <Conversation />
+      <Conversation />
+      <Conversation />
+    </main>
+  );
+};
+
+export default InboxPage;
