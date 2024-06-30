3e6bdf498bf666328d68b7e43c896487670f4828
leobewater <mmbee888@gmail.com>
2024-06-30 14:15:22 -0400

    add property item html

--- djangobnb/app/components/properties/PropertyListItem.tsx
+++ djangobnb/app/components/properties/PropertyListItem.tsx
@@ -8,9 +8,17 @@ const PropertyListItem = () => {
           fill
           src='/beach_1.jpg'
           sizes='(max-width:768px) 768px, (max-width:1200px): 768px, 768px'
-          className='hover:scale-110 object-cover transition h-full w-full' 
+          className='hover:scale-110 object-cover transition h-full w-full'
           alt='Beach House'
         />
+      </div>
+
+      <div className='mt-2'>
+        <p className='text-lg font-bold'>Property name</p>
+      </div>
+
+      <div className="mt-2">
+        <p className="text-sm text-gray-500"><strong>$200</strong> per night</p>
       </div>
     </div>
   );
