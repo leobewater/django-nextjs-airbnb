a8ef84788da531f987997ee783400b7a334985f5
leobewater <mmbee888@gmail.com>
2024-07-01 12:02:58 -0400

    add fields to add property modal

--- djangobnb/app/components/modals/AddPropertyModal.tsx
+++ djangobnb/app/components/modals/AddPropertyModal.tsx
@@ -10,13 +10,15 @@ import Categories from '@/app/components/addproperty/Categories';
 const AddPropertyModal = () => {
   const [currentStep, setCurrentStep] = useState(1);
   const [dataCategory, setDataCategory] = useState('');
+  const [dataTitle, setDataTitle] = useState('');
+  const [dataDescription, setDataDescription] = useState('');
 
   const addPropertyModal = useAddPropertyModal();
 
   // Set data
   const setCategory = (category: string) => {
     setDataCategory(category);
-  }
+  };
 
   const content = (
     <>
@@ -29,8 +31,43 @@ const AddPropertyModal = () => {
           />
           <CustomButton label='Next' onClick={() => setCurrentStep(2)} />
         </>
+      ) : currentStep == 2 ? (
+        <>
+          <h2 className='mb-6 text-2xl'>Describe your place</h2>
+
+          <div className='pt-3 pb-6 space-y-4'>
+            <div className='flex flex-col space-y-2'>
+              <label>Title</label>
+              <input
+                type='text'
+                value={dataTitle}
+                onChange={(e) => setDataTitle(e.target.value)}
+                className='w-full p-4 border border-gray-600 rounded-xl'
+              />
+            </div>
+
+            <div className='flex flex-col space-y-2'>
+              <label>Description</label>
+              <textarea
+                value={dataDescription}
+                onChange={(e) => setDataDescription(e.target.value)}
+                className='w-full h-[200px] p-4 border border-gray-600 rounded-xl'
+              ></textarea>
+            </div>
+          </div>
+
+          <CustomButton
+            label='Previous'
+            onClick={() => setCurrentStep(1)}
+            className='mb-2 bg-black hover:bg-gray-800'
+          />
+
+          <CustomButton label='Next' onClick={() => setCurrentStep(3)} />
+        </>
       ) : (
-        <p>Step2</p>
+        <>
+          <p>Step3</p>
+        </>
       )}
     </>
   );
