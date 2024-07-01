ec9d6708e0088b94956686f9b3e842beea16ca85
leobewater <mmbee888@gmail.com>
2024-07-01 14:06:39 -0400

    get userId in the property detail page

--- djangobnb/app/properties/[id]/page.tsx
+++ djangobnb/app/properties/[id]/page.tsx
@@ -1,9 +1,11 @@
 import ReservationSidebar from '@/app/components/properties/ReservationSidebar';
 import Image from 'next/image';
 import apiService from '@/app/services/apiService';
+import { getUserId } from '@/app/lib/actions';
 
 const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
   const property = await apiService.get(`/api/properties/${params.id}`);
+  const userId = await getUserId();
 
   return (
     <main className='max-w-[1500px] mx-auto px-6'>
@@ -44,7 +46,7 @@ const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
 
           <p className='mt-6 text-lg'>{property.description}</p>
         </div>
-        <ReservationSidebar property={property} />
+        <ReservationSidebar property={property} userId={userId} />
       </div>
     </main>
   );
