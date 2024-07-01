c83bd6d079db8d7bbbee43ac28c362b471da4211
leobewater <mmbee888@gmail.com>
2024-07-01 14:31:16 -0400

    add calendar to reservation sidebar

--- djangobnb/app/components/properties/ReservationSidebar.tsx
+++ djangobnb/app/components/properties/ReservationSidebar.tsx
@@ -2,8 +2,9 @@
 
 import { useState, useEffect } from 'react';
 import { Range } from 'react-date-range';
-import { differenceInDays, eachDayOfInterval } from 'date-fns';
+import { differenceInDays, eachDayOfInterval, format} from 'date-fns';
 
+import DatePicker from '@/app/components/forms/Calendar';
 import apiService from '@/app/services/apiService';
 import useLoginModal from '@/app/hooks/useLoginModal';
 
@@ -38,14 +39,29 @@ const ReservationSidebar = ({ property, userId }: ReservationSidebarProps) => {
     (_, index) => index + 1
   );
 
+  const _setDateRange = (selection: any) => {
+    const newStartDate = new Date(selection.startDate);
+    const newEndDate = new Date(selection.endDate);
+
+    if (newEndDate <= newStartDate) {
+      newEndDate.setDate(newStartDate.getDate() + 1);
+    }
+
+    setDateRange({
+      ...dateRange,
+      startDate: newStartDate,
+      endDate: newEndDate,
+    });
+  };
+
   useEffect(() => {
     if (dateRange.startDate && dateRange.endDate) {
-      const dayCount = differenceInDays(dateRange.startDate, dateRange.endDate);
+      const dayCount = differenceInDays(dateRange.endDate, dateRange.startDate);
 
       if (dayCount && property.price_per_night) {
         const _fee = ((dayCount * property.price_per_night) / 100) * 5;
         setFee(_fee);
-        setTotalPrice(dayCount * property.price_per_night + _fee);
+        setTotalPrice((dayCount * property.price_per_night) + _fee);
         setNights(dayCount);
       } else {
         const _fee = (property.price_per_night / 100) * 5;
@@ -59,6 +75,11 @@ const ReservationSidebar = ({ property, userId }: ReservationSidebarProps) => {
   return (
     <aside className='mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl'>
       <h2 className='mb-5 text-2xl'>${property.price_per_night} per night</h2>
+
+      <DatePicker
+        value={dateRange}
+        onChange={(value) => _setDateRange(value.selection)}
+      />
 
       <div className='mb-6 p-3 border border-gray-400 rounded-xl'>
         <label className='mb-2 block font-bold text-xs'>Guests</label>
