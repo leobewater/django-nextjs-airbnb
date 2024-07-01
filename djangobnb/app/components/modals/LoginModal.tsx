1bc228e208a926a84ea7e7c4301169e32a21cff8
leobewater <mmbee888@gmail.com>
2024-07-01 10:45:03 -0400

    add login logic

--- djangobnb/app/components/modals/LoginModal.tsx
+++ djangobnb/app/components/modals/LoginModal.tsx
@@ -6,32 +6,67 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import useLoginModal from '@/app/hooks/useLoginModal';
 import CustomButton from '@/app/components/forms/CustomButton';
+import apiService from '@/app/services/apiService';
+import { handleLogin } from '@/app/lib/actions';
 
 const LoginModal = () => {
+  const router = useRouter();
   const loginModal = useLoginModal();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState<string[]>([]);
+
+  const submitLogin = async () => {
+    const formData = {
+      email: email,
+      password: password,
+    };
+
+    const response = await apiService.post(
+      '/api/auth/login/',
+      JSON.stringify(formData)
+    );
+    if (response.access) {
+      // store info to cookies
+      handleLogin(response.user.pk, response.access, response.refresh);
+
+      loginModal.close();
+      
+      router.push('/');
+    } else {
+      setErrors(response.non_field_errors);
+    }
+  };
 
   const content = (
     <>
-      <form className='space-y-4'>
+      <form action={submitLogin} className='space-y-4'>
         <input
+          onChange={(e) => setEmail(e.target.value)}
           placeholder='Your email address'
           type='email'
           className='w-full h-[54px] px-4 border border-gray-100 rounded-xl'
         />
+
         <input
+          onChange={(e) => setPassword(e.target.value)}
           placeholder='Your password'
           type='password'
           className='w-full h-[54px] px-4 border border-gray-100 rounded-xl'
         />
 
-        <div className='p-5 bg-airbnb text-white rounded-xl opacity-80'>
-          The error message
-        </div>
+        {errors.map((error, index) => {
+          return (
+            <div
+              key={`error_${index}`}
+              className='p-5 bg-airbnb text-white rounded-xl opacity-80'
+            >
+              {error}
+            </div>
+          );
+        })}
 
-        <CustomButton
-          label='Submit'
-          onClick={() => console.log('login clicked')}
-        />
+        <CustomButton label='Submit' onClick={submitLogin} />
       </form>
     </>
   );
