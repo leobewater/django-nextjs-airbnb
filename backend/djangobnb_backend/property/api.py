229957ee471a80c11112132a5624b09acb297297
leobewater <mmbee888@gmail.com>
2024-07-01 14:39:22 -0400

    add json response after created a booking

--- backend/djangobnb_backend/property/api.py
+++ backend/djangobnb_backend/property/api.py
@@ -68,6 +68,8 @@ def book_property(request, pk):
             guests=guests,
             created_by=request.user
         )
+
+        return JsonResponse({'success': True})
     except Exception as e:
         print('Error', e)
         return JsonResponse({'success': False})
