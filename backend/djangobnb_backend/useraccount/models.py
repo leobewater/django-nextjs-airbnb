f2662c16556445e2b1b50f99f1148ffee0592e85
leobewater <mmbee888@gmail.com>
2024-07-01 10:22:52 -0400

    set up auth register api endpoint

--- backend/djangobnb_backend/useraccount/models.py
+++ backend/djangobnb_backend/useraccount/models.py
@@ -11,7 +11,7 @@ class CustomUserManager(UserManager):
     def _create_user(self, name, email, password, **extra_fields):
         if not email:
             raise ValueError("You have not specified a valid e-mail address")
-    
+
         email = self.normalize_email(email)
         user = self.model(email=email, name=name, **extra_fields)
         user.set_password(password)
