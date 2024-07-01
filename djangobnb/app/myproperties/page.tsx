1820298e885f7a13ecca427d70966ebaa61b52b3
leobewater <mmbee888@gmail.com>
2024-07-01 15:30:55 -0400

    show only my properties

--- djangobnb/app/myproperties/page.tsx
+++ djangobnb/app/myproperties/page.tsx
@@ -1,12 +1,15 @@
 import PropertyList from '@/app/components/properties/PropertyList';
+import { getUserId } from '@/app/lib/actions';
 
-const MyPropertiesPage = () => {
+const MyPropertiesPage = async () => {
+  const userId = await getUserId();
+
   return (
     <main className='max-w-[1500px] mx-auto px-6 pb-6'>
       <h1 className='my-6 text-2xl'>My properties</h1>
 
       <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-        <PropertyList />
+        <PropertyList landlord_id={userId} />
       </div>
     </main>
   );
