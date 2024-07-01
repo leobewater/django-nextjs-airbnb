2a3b407b3c78ef2a05d1b370c99c3f07521afa08
leobewater <mmbee888@gmail.com>
2024-07-01 14:39:36 -0400

    send booking to api

--- djangobnb/app/components/properties/ReservationSidebar.tsx
+++ djangobnb/app/components/properties/ReservationSidebar.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { Range } from 'react-date-range';
-import { differenceInDays, eachDayOfInterval, format} from 'date-fns';
+import { differenceInDays, eachDayOfInterval, format } from 'date-fns';
 
 import DatePicker from '@/app/components/forms/Calendar';
 import apiService from '@/app/services/apiService';
@@ -39,6 +39,37 @@ const ReservationSidebar = ({ property, userId }: ReservationSidebarProps) => {
     (_, index) => index + 1
   );
 
+  const performBooking = async () => {
+    console.log('performBooking', userId);
+
+    if (userId) {
+      if (dateRange.startDate && dateRange.endDate) {
+        const formData = new FormData();
+        formData.append('guests', guests);
+        formData.append(
+          'start_date',
+          format(dateRange.startDate, 'yyyy-MM-dd')
+        );
+        formData.append('end_date', format(dateRange.endDate, 'yyyy-MM-dd'));
+        formData.append('number_of_nights', nights.toString());
+        formData.append('total_price', totalPrice.toString());
+
+        const response = await apiService.post(
+          `/api/properties/${property.id}/book/`,
+          formData
+        );
+
+        if (response.success) {
+          console.log('Booking successful');
+        } else {
+          console.log('Something went wrong...');
+        }
+      }
+    } else {
+      loginModal.open();
+    }
+  };
+
   const _setDateRange = (selection: any) => {
     const newStartDate = new Date(selection.startDate);
     const newEndDate = new Date(selection.endDate);
@@ -61,7 +92,7 @@ const ReservationSidebar = ({ property, userId }: ReservationSidebarProps) => {
       if (dayCount && property.price_per_night) {
         const _fee = ((dayCount * property.price_per_night) / 100) * 5;
         setFee(_fee);
-        setTotalPrice((dayCount * property.price_per_night) + _fee);
+        setTotalPrice(dayCount * property.price_per_night + _fee);
         setNights(dayCount);
       } else {
         const _fee = (property.price_per_night / 100) * 5;
@@ -96,7 +127,10 @@ const ReservationSidebar = ({ property, userId }: ReservationSidebarProps) => {
         </select>
       </div>
 
-      <div className='w-full mb-6 py-6 text-center text-white bg-airbnb hover:bg-airbnb-dark rounded-xl'>
+      <div
+        onClick={performBooking}
+        className='w-full mb-6 py-6 text-center text-white bg-airbnb hover:bg-airbnb-dark rounded-xl'
+      >
         Book
       </div>
 
