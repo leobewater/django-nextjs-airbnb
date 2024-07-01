7f082f9ccb5967eb2cd9e0fa8c772bc9831e43b8
leobewater <mmbee888@gmail.com>
2024-07-01 10:17:29 -0400

    submit signup modal

--- djangobnb/app/components/modals/SignupModal.tsx
+++ djangobnb/app/components/modals/SignupModal.tsx
@@ -6,35 +6,77 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import CustomButton from '@/app/components/forms/CustomButton';
 import useSignupModal from '@/app/hooks/useSignupModal';
+import apiService from '@/app/services/apiService';
 
 const SignupModal = () => {
-  const SignupModal = useSignupModal();
+  const router = useRouter();
+  const signupModal = useSignupModal();
+  const [email, setEmail] = useState('');
+  const [password1, setPassword1] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [errors, setErrors] = useState<string[]>([]);
+
+  // Submit function
+  const submitSignup = async () => {
+    const formData = {
+      email: email,
+      password1: password1,
+      password2: password2,
+    };
+
+    const response = await apiService.post('/api/auth/register/', formData);
+    if (response.access) {
+      // handleLogin
+
+      signupModal.close();
+      router.push('/');
+    } else {
+      const tmpErrors: string[] = Object.values(response).map((error: any) => {
+        return error;
+      });
+
+      setErrors(tmpErrors);
+    }
+  };
 
   const content = (
     <>
-      <form className='space-y-4'>
+      <form action={submitSignup} className='space-y-4'>
         <input
+          onChange={(e) => setEmail(e.target.value)}
           placeholder='Your email address'
           type='email'
           className='w-full h-[54px] px-4 border border-gray-100 rounded-xl'
         />
+
         <input
+          onChange={(e) => setPassword1(e.target.value)}
           placeholder='Your password'
           type='password'
           className='w-full h-[54px] px-4 border border-gray-100 rounded-xl'
         />
+
         <input
+          onChange={(e) => setPassword2(e.target.value)}
           placeholder='Repeat password'
           type='password'
           className='w-full h-[54px] px-4 border border-gray-100 rounded-xl'
         />
-        <div className='p-5 bg-airbnb text-white rounded-xl opacity-80'>
-          The error message
-        </div>
+
+        {errors.map((error, index) => {
+          return (
+            <div
+              key={`error_${index}`}
+              className='p-5 bg-airbnb text-white rounded-xl opacity-80'
+            >
+              {error}
+            </div>
+          );
+        })}
 
         <CustomButton
           label='Submit'
-          onClick={() => console.log('login clicked')}
+          onClick={submitSignup}
         />
       </form>
     </>
@@ -42,8 +84,8 @@ const SignupModal = () => {
 
   return (
     <Modal
-      isOpen={SignupModal.isOpen}
-      close={SignupModal.close}
+      isOpen={signupModal.isOpen}
+      close={signupModal.close}
       label='Sign Up'
       content={content}
     />
