bf54a3d2a36600d8884bb6ea97317b271cb94a1c
leobewater <mmbee888@gmail.com>
2024-07-01 07:55:08 -0400

    fetch properties from api

--- djangobnb/app/components/properties/PropertyList.tsx
+++ djangobnb/app/components/properties/PropertyList.tsx
@@ -1,14 +1,31 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import PropertyListItem from '@/app/components/properties/PropertyListItem';
 
 const PropertyList = () => {
+  const [properties, setProperties] = useState([]);
+
+  const getProperties = async () => {
+    const url = 'http://localhost:8000/api/properties/';
+    await fetch(url, { method: 'GET' })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log('json', json);
+        setProperties(json.data);
+      })
+      .catch((error) => console.log('error', error));
+  };
+
+  useEffect(() => {
+    getProperties();
+  }, []);
+
   return (
     <>
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
+      {properties.map((property) => {
+        return <PropertyListItem />;
+      })}
     </>
   );
 };
