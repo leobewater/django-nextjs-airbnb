49fc6abca1e36f311f0f44e7af7e9dbf56b5157f
leobewater <mmbee888@gmail.com>
2024-07-01 15:22:53 -0400

    add landlord detail api endpoint

---
+++ backend/djangobnb_backend/useraccount/api.py
@@ -0,0 +1,19 @@
+from django.http import JsonResponse
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
+
+from .models import User
+from .serializers import UserDetailSerializer
+
+
+@api_view(['GET'])  # accept GET only
+@authentication_classes([])  # guest allowed
+@permission_classes([])  # no
+def landlord_detail(request, pk):
+    user = User.objects.get(pk=pk)
+    serializer = UserDetailSerializer(user, many=False)
+
+    return JsonResponse(serializer.data, safe=False)
--- backend/djangobnb_backend/useraccount/urls.py
+++ backend/djangobnb_backend/useraccount/urls.py
@@ -4,8 +4,11 @@ from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from django.urls import path
 from rest_framework_simplejwt.views import TokenVerifyView
 
+from . import api
+
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='rest_register'),
     path('login/', LoginView.as_view(), name='rest_login'),
     path('logout/', LoginView.as_view(), name='rest_logout'),
+    path('<uuid:pk>/', api.landlord_detail, name='api_landlord_detail'),
 ]
