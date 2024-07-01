a66a2719e58ec2b0dafd1fbb186b4cd36b60d79b
leobewater <mmbee888@gmail.com>
2024-07-01 11:42:03 -0400

    add steps

--- djangobnb/app/components/modals/AddPropertyModal.tsx
+++ djangobnb/app/components/modals/AddPropertyModal.tsx
@@ -1,11 +1,27 @@
 'use client';
 
+import Image from 'next/image';
+import CustomButton from '@/app/components/forms/CustomButton';
 import Modal from '@/app/components/modals/Modal';
 import useAddPropertyModal from '@/app/hooks/useAddPropertyModal';
-import Image from 'next/image';
+import { useState } from 'react';
 
 const AddPropertyModal = () => {
+  const [currentStep, setCurrentStep] = useState(1);
   const addPropertyModal = useAddPropertyModal();
+
+  const content = (
+    <>
+      {currentStep == 1 ? (
+        <>
+          <h2 className='mb-6 text-2xl'>Choose category</h2>
+          <CustomButton label='Next' onClick={() => setCurrentStep(2)} />
+        </>
+      ) : (
+        <p>Step2</p>
+      )}
+    </>
+  );
 
   return (
     <>
@@ -13,7 +29,7 @@ const AddPropertyModal = () => {
         isOpen={addPropertyModal.isOpen}
         close={addPropertyModal.close}
         label='Add Property'
-        content={(<p>Hello</p>)}
+        content={content}
       />
     </>
   );
