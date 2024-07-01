e5c36292cd18c6eceaf6582c047b4f7cd7b53f05
leobewater <mmbee888@gmail.com>
2024-07-01 15:24:01 -0400

    populate landlord detail

--- djangobnb/app/properties/[id]/page.tsx
+++ djangobnb/app/properties/[id]/page.tsx
@@ -2,6 +2,7 @@ import ReservationSidebar from '@/app/components/properties/ReservationSidebar';
 import Image from 'next/image';
 import apiService from '@/app/services/apiService';
 import { getUserId } from '@/app/lib/actions';
+import Link from 'next/link';
 
 const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
   const property = await apiService.get(`/api/properties/${params.id}`);
@@ -25,8 +26,13 @@ const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
             {property.guests} guests - {property.bedrooms} bedrooms -{' '}
             {property.bathrooms} bathrooms
           </span>
+
           <hr />
-          <div className='py-6 flex items-center space-x-4'>
+
+          <Link
+            href={`/landlords/${property.landlord.id}`}
+            className='py-6 flex items-center space-x-4'
+          >
             {property.landlord.avatar_url && (
               <Image
                 src={property.landlord.avatar_url}
@@ -40,7 +46,7 @@ const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
             <p>
               <strong>{property.landlord.name}</strong> is your host
             </p>
-          </div>
+          </Link>
 
           <hr />
 
