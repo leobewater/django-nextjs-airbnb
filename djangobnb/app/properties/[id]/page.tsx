0a94c1cedd5d2b151e131cfa2f61b8559ef124df
leobewater <mmbee888@gmail.com>
2024-07-01 13:02:34 -0400

    populate property detail page

--- djangobnb/app/properties/[id]/page.tsx
+++ djangobnb/app/properties/[id]/page.tsx
@@ -1,47 +1,46 @@
 import ReservationSidebar from '@/app/components/properties/ReservationSidebar';
 import Image from 'next/image';
+import apiService from '@/app/services/apiService';
 
-const PropertyDetailPage = () => {
+const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
+  const property = await apiService.get(`/api/properties/${params.id}`);
+
   return (
     <main className='max-w-[1500px] mx-auto px-6'>
       <div className='w-full h-[64vh] overflow-hidden rounded-xl relative'>
         <Image
-          src='/beach_1.jpg'
+          src={property.image_url}
           fill
           className='object-cover w-full h-full'
-          alt='Beach house'
+          alt={property.title}
         />
       </div>
 
       <div className='mt-4 grid grid-cols-1 md:grid-cols-5 gap-4'>
         <div className='py-6 pr-6 col-span-3'>
-          <h1 className='mb-4 text-4xl'>Property name</h1>
+          <h1 className='mb-4 text-4xl'>{property.title}</h1>
           <span className='mb-6 block text-lg text-gray-600'>
-            4 guests - 2 bedrooms - 1 bathroom
+            {property.guests} guests - {property.bedrooms} bedrooms -{' '}
+            {property.bathrooms} bathrooms
           </span>
           <hr />
           <div className='py-6 flex items-center space-x-4'>
             <Image
-              src='/profile_pic_1.jpg'
+              src={property.landlord.avatar_url}
               width={50}
               height={50}
               className='rounded-full'
-              alt='The user name'
+              alt={property.landlord.name}
             />
 
             <p>
-              <strong>John Doe</strong> is your host
+              <strong>{property.landlord.name}</strong> is your host
             </p>
           </div>
 
           <hr />
 
-          <p className='mt-6 text-lg'>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
-            eum, numquam quaerat enim aperiam illo hic voluptatum iste explicabo
-            quam commodi nulla sint magni sapiente voluptas reprehenderit
-            necessitatibus officiis doloremque.
-          </p>
+          <p className='mt-6 text-lg'>{property.description}</p>
         </div>
         <ReservationSidebar />
       </div>
